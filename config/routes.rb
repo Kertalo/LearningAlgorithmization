f00765453d5bd3046157aae5b0cc1c228b6d75ab
Rogalik <koksharov-kir@yandex.ru>
2023-04-12 19:23:15 +0300

    add registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
+  get 'persons/profile'
   root 'menus#index'
+
+  #get 'users/sign_out', to: root_path
+  get 'persons/profile', as: 'user_root'
 
   get '/update_level', to: 'levels#update_level'
   post '/run_program', to: 'levels#run_program'
